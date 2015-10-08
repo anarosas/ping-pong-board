718c072836cf07c3dcf187e2e5dd0e28a040de99
Ana Rosas <a.rosas10@gmail.com>
2015-10-08 15:03:08 -0300

    Remove authenticate

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,5 @@
 class GamesController < ApplicationController
   before_action :set_game, only: [:show, :edit, :update, :destroy]
-  before_filter :authenticate_user!
   # GET /games
   # GET /games.json
   def index
